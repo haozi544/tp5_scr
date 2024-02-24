d3a4c95f88d5d31d5c206ef6ee98632047a9f581
haozi544 <55656492+haozi544@users.noreply.github.com>
2024-02-24 09:32:19 +0800

    Add files via upload

---
+++ vendor/ali/Sms.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PHPStorm
+ * User whh
+ * Date: 2023/2/12
+ * Time: 15:54
+ */
+
+namespace ali;
+
+class Sms
+{
+    public static function send()
+    {
+        echo 'sms';
+    }
+}
---
+++ vendor/composer/installed.php
@@ -0,0 +1,51 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'topthink/think',
+  ),
+  'versions' => 
+  array (
+    'topthink/framework' => 
+    array (
+      'pretty_version' => 'v5.1.41',
+      'version' => '5.1.41.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '7137741a323a4a60cfca334507cd1812fac91bb2',
+    ),
+    'topthink/think' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'topthink/think-helper' => 
+    array (
+      'pretty_version' => 'v3.1.6',
+      'version' => '3.1.6.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '769acbe50a4274327162f9c68ec2e89a38eb2aff',
+    ),
+    'topthink/think-installer' => 
+    array (
+      'pretty_version' => 'v2.0.5',
+      'version' => '2.0.5.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '38ba647706e35d6704b5d370c06f8a160b635f88',
+    ),
+  ),
+);
